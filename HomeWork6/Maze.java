8cd749bf25fd11717b36586bd2aab6bcb507eca2
hasslehub <66742510+hasslehub@users.noreply.github.com>
2023-03-04 16:04:08 +0300

    Update Maze.java

--- HomeWork6/Maze.java
+++ HomeWork6/Maze.java
@@ -118,38 +118,6 @@ class MapPrinter {
       }
       return sb.toString();
     }
-  
-
-    
-public String mapColor(int[][] map) {
-    StringBuilder sb = new StringBuilder();
-  
-    for (int row = 0; row < map.length; row++) {
-    for (int col = 0; col < map[row].length; col++) {
-        switch (map[row][col]) {
-        case 0:
-            sb.append("░░░");
-            break;
-        case -1:
-            sb.append("▓▓▓");
-            break;
-        case -2:
-            sb.append("R");
-            break;
-        case -3:
-            sb.append("E");
-            break;
-        default:
-            break;
-        }
-    }
-    sb.append("\n");
-    }
-    for (int i = 0; i < 3; i++) {
-        sb.append("\n");
-    }
-    return sb.toString();
-    }
 }
 
 
